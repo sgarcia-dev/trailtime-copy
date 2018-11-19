26299c45710bc5fbf713a582aa820700573de020
Lesley <lesleydreyer@hotmail.com>
2018-11-18 17:54:12 -0700

    worked on home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import './index.css';
+import logo from './logo.svg';
 
 const HomePage = ({ history }) => {
     return (
-        <div>
-            <header role="banner">
-                <div className="heroPic">picture in here</div>
-                <h1>Trail Time</h1>
-                <h5>(or Trail Log, or Jeep Meet)</h5>
-                <div className="row">
-                </div>
-            </header>
-            <main role="main">
-                <div>
-                    <p>Discover new trails, log your photos, and meet up with others.</p><br />
-                    <button className="loginbtn" onClick={() => history.push('/login')} type="button">Log In</button>
-                    <button className="signupbtn" onClick={() => history.push('/signup')} type="button">Sign Up</button>
-                </div><br /><br />
-            </main>
-        </div>
+        <React.Fragment>
+            <div className='bg'>
+                <header role="banner">
+                    <img src={logo} />
+                    <div className="row">
+                    </div>
+                </header>
+                <main role="main">
+                    <div>
+                        <span className="circle">Discover new trails</span>
+                        <span className="circle">Add your trails</span>
+                        <span className="circle">Meet up with others</span>
+                        <button className="loginbtn" onClick={() => history.push('/login')} type="button">Start Adventuring > </button>
+                    </div><br /><br />
+                </main>
+            </div>
+        </React.Fragment>
     )
 }
 
